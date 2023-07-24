4f6ecb722394578fd2c6d10bbc2eea44ac550160
unknown <vstroganov.reon@gmail.com>
2023-07-24 18:07:43 +0300

    wip: auth start

--- src/shared/stores/index.ts
+++ src/shared/stores/index.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../../pages/SignUp/widgets/SignUpForm/store/index'
 
 
 const store = configureStore({
   reducer: {
+    auth: authReducer
   },
 });
 
